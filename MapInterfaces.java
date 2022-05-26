d948f9a4be332934901678a4122ee9a0cf372f9d
fathimamol <94897169+fathimamol@users.noreply.github.com>
2022-05-26 14:50:15 +0530

    Eleventh commit

---
+++ MapInterfaces.java
@@ -0,0 +1,48 @@
+import java.util.*;
+class MapInterfaces
+{
+public static void main(String args[])
+{
+System.out.println("Adding Elements");
+Map<Integer, String> hm1 = new HashMap<>();
+Map<Integer, String> hm2= new HashMap<Integer, String>();
+hm1.put(1, "Adding 1");
+hm1.put(2, "Adding 2");
+hm1.put(3, "Adding 3");
+hm2.put(new Integer(1), "value 1");
+hm2.put(new Integer(2), "value 2");
+hm2.put(new Integer(3), "value 3");
+System.out.println(hm1);
+System.out.println(hm2);
+System.out.println("Changing Elements");
+Map<Integer, String> hm3= new HashMap<Integer, String>();
+hm3.put(new Integer(1), "ram");
+hm3.put(new Integer(2), "lost");
+hm3.put(new Integer(3), "raj");
+System.out.println("Initial Map " + hm3);  
+hm3.put(new Integer(2), "For");  
+System.out.println("Updated Map " + hm3);
+System.out.println("Removing Elements");	
+Map<Integer, String> hm4= new HashMap<Integer, String>();
+hm4.put(new Integer(1), "Fathima");
+hm4.put(new Integer(2), "Preema");
+hm4.put(new Integer(3), "Reshma");
+hm4.put(new Integer(4), "Salshi");
+System.out.println(hm4);
+hm4.remove(new Integer(4));
+System.out.println(hm4);
+}
+}
+
+Output
+
+
+Adding Elements
+{1=Adding 1, 2=Adding 2, 3=Adding 3}
+{1=value 1, 2=value 2, 3=value 3}
+Changing Elements
+Initial Map {1=ram, 2=lost, 3=raj}
+Updated Map {1=ram, 2=For, 3=raj}
+Removing Elements
+{1=Fathima, 2=Preema, 3=Reshma, 4=Salshi}
+{1=Fathima, 2=Preema, 3=Reshma}
---
+++ arrayList.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class arrayList
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the number of strings: ");
+int max = sc.nextInt();
+List<String> list = new ArrayList<String>();
+System.out.println("Enter the strings: ");
+for(int i=0; i<max; i++)
+list.add(sc.next());
+list.set(1,"Fathima");
+Iterator itr = list.iterator();
+System.out.println("Entered Strings are: ");
+while(itr.hasNext()){
+System.out.println(itr.next());
+}
+}
+}
+
+Output
+
+Enter the number of strings:
+3
+Enter the strings:
+preema
+sahla
+salshi
+Entered Strings are:
+preema
+Fathima
+salshi
